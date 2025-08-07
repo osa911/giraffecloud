a870905826da1ac7ca4085e9448315824cf7b847
osa911 <e.osadchuy@gmail.com>
2025-08-07 15:50:48 +0100

    test version update

--- cmd/giraffecloud/main.go
+++ cmd/giraffecloud/main.go
@@ -387,7 +387,7 @@ func checkVersionCompatibility(serverAddr string) {
 func init() {
 	// Initialize logger first
 	initLogger()
-	logger.Info("Initializing GiraffeCloud CLI")
+	logger.Info("🦒 Initializing GiraffeCloud CLI")
 
 	// Setup core commands
 	rootCmd.AddCommand(connectCmd)
