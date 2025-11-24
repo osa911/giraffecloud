7adb6a40f30c7df3bfda1f1d0bfe2bc97729e442
osa911 <e.osadchuy@gmail.com>
2025-11-24 22:44:31 +0000

    feat: configure Next.js settings for improved security and performance

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,1 +1,54 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'], // For Google Auth profile pictures
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://api.giraffecloud.xyz https://identitytoolkit.googleapis.com https://securetoken.googleapis.com; frame-src 'self' https://giraffecloud.xyz;"
+          }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = nextConfig
